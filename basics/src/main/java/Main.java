a7a244c4a0bd11e9423c587b066951785748b889
Zahra <zahram1087@gmail.com>
2018-11-27 11:49:26 -0800

    finished lab1

--- basics/src/main/java/Main.java
+++ basics/src/main/java/Main.java
@@ -1,17 +1,18 @@
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
-//        pluralize ("cat", 1 );
-//        flipNHeads(1);
+        pluralize ("cat", 1 );
+        flipNHeads(3);
         clock();
 
 
     }
 
     public static void pluralize(String name, int count) {
-        if (count >= 0 && count != 1) {
+        if (count >= 0 && count != 1 ) {
             System.out.println("I own " + count + " " + name + "s");
         } else {
             System.out.println("I own " + count + " " + name);
@@ -19,29 +20,35 @@ public class Main {
     }
 
     public static void flipNHeads(int n) {
-//
+        String name;
         int headscounter = 0;
-//
-        do {
-            int num = ThreadLocalRandom.current().nextInt(0, 2);
-//            headscounter++;
-            if (num < .5) {
-                headscounter++;
-                System.out.println("heads");
+        int flips= 0;
 
-            } else {
-                System.out.println("Tails");
+        while (headscounter < n) {
+            int num = ThreadLocalRandom.current().nextInt(0, 2);
+
+            if (num < .5 ) {
+                name = "heads";
+                headscounter++;
+                flips++;
+                System.out.println(name);
+            }
+            else {
+                name = "tails";
+                flips++;
+                headscounter=0;
+                System.out.println(name);
             }
 
-        } while (headscounter < n);
-        System.out.println("It took " + headscounter + " to flips " + n + " in a row");
+        } ; // !name.equals("heads")
+        System.out.println("It took " +flips + " to flips " + n + " heads in a row");
     }
 
     public static void clock() {
 
     //Thread.sleep in Java. Thread.sleep() method can be used to pause the
         // execution of current thread for specified time in milliseconds
-        //thread to suspend execution for a specified period. 
+        //thread to suspend execution for a specified period.
 
         Thread clock = new Thread()
         {
@@ -53,7 +60,9 @@ public class Main {
                         int hour = now.getHour();
                         int minute = now.getMinute();
                         int second = now.getSecond();
-                        System.out.println("Time: " + hour + ": " + minute + ": " + (double)second);
+                        String time = now.format(DateTimeFormatter.ofPattern("HH:mm:ss"));
+                        String.format(time, hour, minute,second);
+                        System.out.println(time);
 
                         sleep(1000);
                     }
