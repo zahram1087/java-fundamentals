9d6638dfb445e800e335879f497e42306a67e1d3
Zahra <zahram1087@gmail.com>
2018-11-30 11:14:36 -0800

    finished tests for all labs

---
+++ bitmap-transformer/src/main/java/Bitmap.java
@@ -0,0 +1,19 @@
+import java.nio.file.Path;
+
+public class Bitmap {
+Path inoutFilePath;
+Path outPutFilePath;
+String transfromName;
+int rgb;
+int red;
+int green;
+int blue;
+int height;
+int width;
+    //CONSTRUCTOR
+    public Bitmap(Path inoutFilePath, Path outPutFilePath, String transfromName){
+        this.inoutFilePath = inoutFilePath;
+        this.outPutFilePath = outPutFilePath;
+        this.transfromName = transfromName;
+    }
+}
